1c7df5f003b7931a89026b7efbdd882bec3377b5
Gordon Smith <GordonJSmith@gmail.com>
2023-04-29 08:17:03 +0100

    WIP

--- scripts/cpp-build.sh
+++ scripts/cpp-build.sh
@@ -5,7 +5,6 @@ then
     mkdir -p build/src-components
     cmake -S ./src-components -B ./build/src-components -G "Ninja" -DCMAKE_BUILD_TYPE=MinSizeRel
 fi
-
 cmake --build ./build/src-components --parallel
 
 source ./emsdk/emsdk_env.sh
@@ -14,7 +13,6 @@ then
     mkdir -p build/src-cpp
     cmake -S ./src-cpp -B ./build/src-cpp -DCMAKE_BUILD_TYPE=MinSizeRel
 fi
-
 mkdir -p ./lib-esm
 cmake --build ./build/src-cpp -- -j
 
