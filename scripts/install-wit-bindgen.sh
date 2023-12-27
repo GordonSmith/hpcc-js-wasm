e1fc583612979ef10aaf5621301435f7e4be3ab2
Gordon Smith <GordonJSmith@gmail.com>
2023-12-27 07:56:20 +0100

    WIP
    WIP

---
+++ scripts/install-wit-bindgen.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# List of current vertsion can be found in https://github.com/bytecodealliance/wit-bindgen/releases  ---
+VERSION=0.16.0
+
+if [ ! -d "./wit-bindgen" ] 
+then
+    mkdir wit-bindgen
+
+    curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
+    cargo install wasm-tools
+
+    cargo install --git https://github.com/bytecodealliance/wit-bindgen --tag wit-bindgen-cli-$VERSION wit-bindgen-cli
+
+    curl https://wasmtime.dev/install.sh -sSf | bash
+
+    wget https://github.com/bytecodealliance/wasmtime/releases/download/v16.0.0/wasi_snapshot_preview1.command.wasm
+    mv ./wasi_snapshot_preview1.command.wasm ./wit-bindgen
+
+    wget https://github.com/bytecodealliance/wasmtime/releases/download/v16.0.0/wasi_snapshot_preview1.reactor.wasm
+    mv ./wasi_snapshot_preview1.reactor.wasm ./wit-bindgen
+
+    wget https://github.com/bytecodealliance/wasmtime/releases/download/v16.0.0/wasi_snapshot_preview1.proxy.wasm
+    mv ./wasi_snapshot_preview1.proxy.wasm ./wit-bindgen
+fi
