e53703d4889753d2cc2942ce200970481d588aab
Gordon Smith <GordonJSmith@gmail.com>
2024-08-11 12:16:07 +0100

    WIP

--- packages/duckdb/esbuild.mjs
+++
@@ -1,9 +0,0 @@
-import { browserTpl, neutralTpl, nodeTpl } from "@hpcc-js/esbuild-plugins";
-
-//  config  ---
-await browserTpl("src/index.ts", "dist/index");
-await Promise.all([
-    browserTpl("test/index-browser.ts", "dist-test/index.browser"),
-    // nodeTpl("test/index-node.ts", "dist-test/index.node"),
-]);
-
